aa3957f96cc0f60cf4e3c2114afddae1fd72fb11
Adam Essmyer <acemyer@Adams-MacBook-Pro.local>
2018-04-19 18:18:13 -0500

    submitted app and Todo

---
+++ react-to-do/src/components/ToDo.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+
+class ToDo extends Component {
+  render() {
+    return (
+      <li> {this.props.description} </li>
+    )
+  }
+}
+export default ToDo;
