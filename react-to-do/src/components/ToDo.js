8753118d3d328d09fbef8e1e7daedbf9dfdcdb7a
Adam Essmyer <acemyer@Adams-MacBook-Pro.local>
2018-04-23 17:28:27 -0500

    made final changes

--- react-to-do/src/components/ToDo.js
+++ react-to-do/src/components/ToDo.js
@@ -3,7 +3,9 @@ import React, {Component} from 'react';
 class ToDo extends Component {
   render() {
     return (
-      <li> {this.props.description} </li>
+      <li>
+      <input type="checkbox" checked={this.props.isCompleted} />
+      </li>
     )
   }
 }
